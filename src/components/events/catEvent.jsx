35b3eb904d680c46b4ae3ce9e044848d6840475c
ysk <103978285+infinitumwrld@users.noreply.github.com>
2023-01-30 00:13:01 -0500

    Update catEvent.jsx
    Updated with the code explanation for better understanding.

--- src/components/events/catEvent.jsx
+++ src/components/events/catEvent.jsx
@@ -23,3 +23,17 @@ const CatEvent = ({ data, pageName }) => {
 };
 
 export default CatEvent;
+
+/* CODE EXPLANATION:
+This component CatEvent is a stateless component that takes two props: data and pageName. It is responsible for 
+rendering a list of events and displaying information about the events in a card format.
+
+The component displays a title using an h1 element with text 'Events in' and pageName passed as a prop. 
+The component maps over the data prop and for each event, it creates a link component that takes the user to 
+a page that shows more details about the event. The link component has an a element with class name card which is 
+styled to look like a card. Inside the a element, there is an Image component from the next/image library, which displays
+an image of the event with the specified width, height, and alt text. The component also displays the title and description
+of the event in h2 and p elements, respectively.
+
+
+*/
